99d069776e2080ab9d0ab62276fe89fe04b50138
sumnic <sum_ith@mail.ru>
2025-07-22 21:36:24 +0300

    Исправление README.md

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -4,24 +4,24 @@ const parserTs = require('@typescript-eslint/parser');
 const prettierPlugin = require('eslint-plugin-prettier');
 
 module.exports = [
-  {
-    ignores: ['node_modules', 'dist', 'prisma', '.env'],
-    files: ['**/*.ts'],
-    languageOptions: {
-      parser: parserTs,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
-    },
-    plugins: {
-      '@typescript-eslint': eslintPluginTs,
-      prettier: prettierPlugin,
-    },
-    rules: {
-      semi: ['error', 'always'],
-      quotes: ['error', 'single'],
-      'prettier/prettier': 'error',
-    },
-  },
+	{
+		ignores: ['node_modules', 'dist', 'prisma', '.env'],
+		files: ['**/*.ts'],
+		languageOptions: {
+			parser: parserTs,
+			parserOptions: {
+				ecmaVersion: 'latest',
+				sourceType: 'module',
+			},
+		},
+		plugins: {
+			'@typescript-eslint': eslintPluginTs,
+			prettier: prettierPlugin,
+		},
+		rules: {
+			semi: ['error', 'always'],
+			quotes: ['error', 'single'],
+			'prettier/prettier': 'error',
+		},
+	},
 ];
